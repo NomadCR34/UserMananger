e02c4f6d07d1265398dc5626a09595a7208e3042
Amin Rouholamini <nomad.cr34@gmail.com>
2022-06-14 20:35:33 +0430

    created project

---
+++ src/main/java/ir/amin/app/usermanager/UserManagerApplication.java
@@ -0,0 +1,13 @@
+package ir.amin.app.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/amin/app/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.amin.app.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
